89ba5ed5d98ffd65265f86f1902645b497f82bb3
Younis Tamer <younistamer@yahoo.com>
2023-08-06 14:51:26 +0300

    Array-Ex4

---
+++ Unit 2/Array and strings/Array-Ex4/src/Array-Ex4.c
@@ -0,0 +1,63 @@
+/*
+ ============================================================================
+ Name        : Array-Ex4.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+
+	int arr[10];
+	int elements;
+	int inserted;
+	int location;
+
+	printf("Enter no of elements: ");
+	fflush(stdin);fflush(stdout);
+	scanf("%d",&elements);
+
+	for(int i=0;i<elements;i++)
+	{
+		scanf("%d",&arr[i]);
+
+	}
+
+
+	//	for(int i=0;i<elements;i++)
+	//	{
+	//		printf("%d ",arr[i]);
+	//	}
+
+	printf("Enter the element to be inserted: ");
+	fflush(stdin);fflush(stdout);
+	scanf("%d",&inserted);
+
+	printf("Enter the location: ");
+	fflush(stdin);fflush(stdout);
+	scanf("%d",&location);
+
+
+	for(int i=elements;i>0;i--)
+	{
+		arr[i]=arr[i-1];
+		if(i==location-1)
+			arr[i]=inserted;
+	}
+
+		for(int i=0;i<elements+1;i++)
+			{
+				printf("%d ",arr[i]);
+			}
+
+
+
+
+	return EXIT_SUCCESS;
+}
