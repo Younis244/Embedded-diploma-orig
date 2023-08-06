9335803f1d07f14efd9b4eb6dd71317b1f88278b
Younis Tamer <younistamer@yahoo.com>
2023-08-06 15:24:00 +0300

    Array Ex5

---
+++ Unit 2/Array and strings/Array-Ex5/src/Array-Ex5.c
@@ -0,0 +1,44 @@
+/*
+ ============================================================================
+ Name        : Array-Ex5.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+	int arr[10];
+	int elements;
+	int search;
+	int location;
+
+	printf("Enter no of elements: ");
+	fflush(stdin);fflush(stdout);
+	scanf("%d",&elements);
+
+	for(int i=0;i<elements;i++)
+	{
+		scanf("%d",&arr[i]);
+	}
+
+	printf("Enter the elements to be searched: ");
+	fflush(stdin);fflush(stdout);
+	scanf("%d",&search);
+
+	for(int j=0;j<elements;j++)
+	{
+		if(arr[j]==search)
+		location=j+1;
+	}
+
+	printf("Number found at locaion: %d",location);
+
+
+	return EXIT_SUCCESS;
+}
