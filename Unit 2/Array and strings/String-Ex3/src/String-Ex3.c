a840a87f050417af2c4da5894481043f340d86a0
Younis Tamer <younistamer@yahoo.com>
2023-08-07 16:06:33 +0300

    String Ex3

--- Unit 2/Array and strings/String-Ex3/src/String-Ex3.c
+++ Unit 2/Array and strings/String-Ex3/src/String-Ex3.c
@@ -11,7 +11,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+int main(void)
+{
+
+	char sentence[100];
+	char temp;
+	int i,j;
+
+	printf("Enter the string: ");
+	fflush(stdin);fflush(stdout);
+	gets(sentence);
+
+	i=0;
+	j=strlen(sentence)-1;
+
+			while(i<j)
+			{
+				temp=sentence[i];
+				sentence[i]=sentence[j];
+				sentence[j]=temp;
+				i++;
+				j--;
+			}
+	printf("Reverse string is: %s",sentence);
+
+
+
+
+
 	return EXIT_SUCCESS;
 }
