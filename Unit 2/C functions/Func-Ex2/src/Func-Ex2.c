89eac00312470ce3d38af6e0343246f66ab6fa2b
Younis Tamer <younistamer@yahoo.com>
2023-08-15 02:54:54 +0300

    Ex 2 function

---
+++ Unit 2/C functions/Func-Ex2/src/Func-Ex2.c
@@ -0,0 +1,39 @@
+/*
+ ============================================================================
+ Name        : Func-Ex2.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+int result=1;
+int factorial(int number)
+{
+	result*=number;
+	number--;
+	if (number==1)
+			return result;
+
+	factorial(number);
+
+}
+
+int main()
+{
+
+	int n;
+	printf("Enter an positive number: ");
+	fflush(stdin);fflush(stdout);
+	scanf("%d",&n);
+
+
+	printf("fctorial(%d)=%d ",n,factorial(n));
+
+
+
+	return 0;
+}
