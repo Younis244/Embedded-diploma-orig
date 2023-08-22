c87b279cac9e5df88cd69153ed80819a0f4a56b8
Younis Tamer <younistamer@yahoo.com>
2023-08-22 16:12:27 +0300

    Q3 mid

---
+++ Unit 2/Midterm(Term1)/Q3-Mid/src/Q3-Mid.c
@@ -0,0 +1,70 @@
+/*
+ ============================================================================
+ Name        : Q3-Mid.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+void PrimeKnower(int start,int end)
+{
+	int arr[100];
+	int j=0;
+	int x=0;
+	for(int i=start;i<=end;i++)
+	{
+
+		if(i==1||i==2||i==3||i==5)
+		{
+			arr[j]=i;
+			j++;
+		}
+
+		else if(i%2==0)
+		{
+			continue;
+		}
+		else if(i%3==0)
+		{
+			continue;
+		}
+		else if(i%5==0)
+		{
+			continue;
+		}
+		else
+		{
+			arr[j]=i;
+			j++;
+		}
+		x=j;
+
+	}
+	printf("Prime numbers betn %d and %d are: ",start,end);
+	for(int k=0;k<x;k++)
+	{
+		printf("%d ",arr[k]);
+	}
+}
+
+int main()
+{
+	int bedaya,nehaya;
+
+	printf("n1: ");
+	fflush(stdin);fflush(stdout);
+	scanf("%d",&bedaya);
+
+	printf("n2: ");
+	fflush(stdin);fflush(stdout);
+	scanf("%d",&nehaya);
+
+	PrimeKnower(bedaya,nehaya);
+
+	return 0;
+}
