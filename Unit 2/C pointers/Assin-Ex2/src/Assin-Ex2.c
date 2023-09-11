3dc5245376c9df98e87c6806824aa3d0ce45a138
Younis Tamer <younistamer@yahoo.com>
2023-09-11 17:42:19 +0300

    Assin-Ex2

---
+++ Unit 2/C pointers/Assin-Ex2/src/Assin-Ex2.c
@@ -0,0 +1,46 @@
+/*
+ ============================================================================
+ Name        : Assin-Ex2.c
+ Author      : Younis Tamer Hosny
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	/*First solution
+	char array[]={'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+	char* ptr=array;
+
+	printf("The alphabet is: \r\n");
+	for(int i=0;i<sizeof(array);i++,ptr++)
+	{
+		printf("%c ",*ptr);
+	}
+*/
+
+	char alphabet[27];
+	char* pointer=alphabet;
+	pointer=alphabet;
+	for(int i=0;i<27;i++,pointer++)
+	{
+		*pointer=i+'A';
+	}
+
+	//pointer=alphabet;
+
+	printf("The alphabet is: \r\n");
+	for(int i=0;i<27;i++,pointer++)
+	{
+		printf("%c ",*pointer);
+	}
+
+
+
+	return 0;
+}
