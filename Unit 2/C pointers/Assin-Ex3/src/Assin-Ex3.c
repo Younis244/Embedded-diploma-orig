3c4ac6c0962bef443d3621caf682cb19d5f2706f
Younis Tamer <younistamer@yahoo.com>
2023-09-11 18:03:18 +0300

    Assin-Ex3

--- Unit 2/C pointers/Assin-Ex3/src/Assin-Ex3.c
+++ Unit 2/C pointers/Assin-Ex3/src/Assin-Ex3.c
@@ -1,7 +1,7 @@
 /*
  ============================================================================
  Name        : Assin-Ex3.c
- Author      : 
+ Author      : Younis Tamer Hosny
  Version     :
  Copyright   : Your copyright notice
  Description : Hello World in C, Ansi-style
@@ -10,8 +10,25 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
-	return EXIT_SUCCESS;
+int main()
+{
+	char arr[100];
+	char* ptr=arr;
+
+	printf("Enter string to reverse: ");
+	fflush(stdin);fflush(stdout);
+	gets(arr);
+
+	int x=strlen(arr)-1;
+	ptr=&arr[x];
+
+	for(int i=strlen(arr);i>0;i--,ptr--)
+	{
+		printf("%c",*ptr);
+	}
+
+
+	return 0;
 }
