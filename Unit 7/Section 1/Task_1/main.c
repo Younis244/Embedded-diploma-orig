6e43a724468c2ddaab601a41fd3cd0f87a986536
Younis Tamer <younistamer@yahoo.com>
2024-09-01 03:28:06 +0300

    Unit7 task 1 assingment

---
+++ Unit 7/Section 1/Task_1/main.c
@@ -0,0 +1,90 @@
+/*
+ * main.c
+ *
+ *  Created on: Sep 1, 2024
+ *      Author: Younis
+ */
+
+
+#define F_CPU 80000000UL
+
+#include <util/delay.h>
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#define Set_bit(register,bit)		register|=(1<<bit)
+//#define Set_bit_hex(register,hex)	register|=(hex)
+#define Toggle_bit(register,bit)	register^=(1<<bit)
+//#define Clear_bit_hex(register,hex)	register&=(hex)
+#define Clear_bit(register,bit)		register&=~(1<<bit)
+
+#define BASE			0x20
+#define DDRD			*(volatile unsigned char*)(BASE + 0x11)
+#define PORT_D			*(volatile unsigned char*)(BASE + 0x12)
+
+GPIO_init()
+{
+	//making pins of portD all output
+	Set_bit(DDRD,0);
+	Set_bit(DDRD,1);
+	Set_bit(DDRD,2);
+	Set_bit(DDRD,3);
+	Set_bit(DDRD,4);
+	Set_bit(DDRD,5);
+	Set_bit(DDRD,6);
+	Set_bit(DDRD,7);
+
+}
+
+GPIO_SET()
+{
+	Set_bit(PORT_D,0);
+	_delay_ms(100);
+	Set_bit(PORT_D,1);
+	_delay_ms(100);
+	Set_bit(PORT_D,2);
+	_delay_ms(100);
+	Set_bit(PORT_D,3);
+	_delay_ms(100);
+	Set_bit(PORT_D,4);
+	_delay_ms(100);
+	Set_bit(PORT_D,5);
+	_delay_ms(100);
+	Set_bit(PORT_D,6);
+	_delay_ms(100);
+	Set_bit(PORT_D,7);
+	_delay_ms(100);
+}
+
+GPIO_RESET()
+{
+	Clear_bit(PORT_D,7);
+	_delay_ms(100);
+	Clear_bit(PORT_D,6);
+	_delay_ms(100);
+	Clear_bit(PORT_D,5);
+	_delay_ms(100);
+	Clear_bit(PORT_D,4);
+	_delay_ms(100);
+	Clear_bit(PORT_D,3);
+	_delay_ms(100);
+	Clear_bit(PORT_D,2);
+	_delay_ms(100);
+	Clear_bit(PORT_D,1);
+	_delay_ms(100);
+	Clear_bit(PORT_D,0);
+	_delay_ms(100);
+}
+
+int main(void)
+{
+	//Initializing pins to be output
+	GPIO_init();
+
+    while (1)
+    {
+    	GPIO_SET();
+    	GPIO_RESET();
+    }
+}
+
