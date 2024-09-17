e498f5d89d5a29808529e4be2e586c204be8b9bc
Younis Tamer <younistamer@yahoo.com>
2024-09-17 17:55:04 +0300

    Unit 7 lesson 3

---
+++ Unit 7/Lesson 3/Drivers/Debug/Stm32_F103C6_Drivers/subdir.mk
@@ -0,0 +1,19 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Stm32_F103C6_Drivers/Stm32_F103C6_gpio_drivers.c 
+
+OBJS += \
+./Stm32_F103C6_Drivers/Stm32_F103C6_gpio_drivers.o 
+
+C_DEPS += \
+./Stm32_F103C6_Drivers/Stm32_F103C6_gpio_drivers.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Stm32_F103C6_Drivers/Stm32_F103C6_gpio_drivers.o: ../Stm32_F103C6_Drivers/Stm32_F103C6_gpio_drivers.c
+	arm-none-eabi-gcc -gdwarf-2 "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DSTM32 -DSTM32F1 -DSTM32F103C6Tx -DDEBUG -c -I"C:/Users/Younis/Desktop/Embedded-diploma-orig/Unit 7/Lesson 3/Drivers/Stm32_F103C6_Drivers/inc" -I"C:/Users/Younis/Desktop/Embedded-diploma-orig/Unit 7/Lesson 3/Drivers/Inc" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Stm32_F103C6_Drivers/Stm32_F103C6_gpio_drivers.d" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
